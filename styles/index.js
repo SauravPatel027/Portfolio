2b1b9700be06d300a57a887efe1f85db2c9b3f26
SauravPatel027 <140683115+SauravPatel027@users.noreply.github.com>
2025-01-15 02:33:09 +0530

    Update index.js

--- styles/index.js
+++ styles/index.js
@@ -16,21 +16,9 @@ const styles = {
   // hero section
   heroHeading:
       'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
-  heroaText:
-`
-  md:w-[300px] sm:w-[200px] w-[150px] 
-  md:h-[120px] sm:h-[80px] h-[60px] 
-  md:border-[20px] border-[10px] 
-  rounded-[50px] 
-  border-white 
-  relative
-  before:content-[""] 
-  before:absolute 
-  before:top-0 
-  before:right-1/2 
-  before:w-[20px] 
-  before:h-full 
-  before:bg-white
-`};
+  heroDText:
+  'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+
+};
 
 export default styles;
